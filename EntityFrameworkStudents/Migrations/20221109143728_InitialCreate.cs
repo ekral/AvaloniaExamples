08a272ad86299bbbf1644bde8b9f13ba753b8468
Erik Král <ekral@utb.cz>
2022-11-09 15:51:50 +0100

    path fix

---
+++ EntityFrameworkStudents/Migrations/20221109143728_InitialCreate.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EntityFrameworkStudents.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Rocnik = table.Column<int>(type: "INTEGER", nullable: false),
+                    Jmeno = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Jmeno", "Rocnik" },
+                values: new object[,]
+                {
+                    { 1, "Bohdan", 2 },
+                    { 2, "Hubert", 2 },
+                    { 3, "Alena", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ EntityFrameworkStudents/Program.cs
@@ -0,0 +1,40 @@
+﻿// https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?tabs=netcore-cli
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+using UniversityContext db = new();
+db.Students.Add(new Student() { Jmeno = "ahoj", Rocnik = 3 });
+
+foreach (Student student in db.Students)
+{
+    Console.WriteLine($"Id: {student.Id, 3} Jmeno: {student.Jmeno, 12} Rocnik: {student.Rocnik, 3}");
+}
+
+Console.WriteLine("konec");
+
+class UniversityContext : DbContext
+{
+    public DbSet<Student> Students { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string folder = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        string path = Path.Join(folder, "database.db");
+        optionsBuilder.UseSqlite($"Data Source={path}");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>().HasData(
+            new Student() { Id = 1, Rocnik = 2, Jmeno = "Bohdan" },
+            new Student() { Id = 2, Rocnik = 2, Jmeno = "Hubert" },
+            new Student() { Id = 3, Rocnik = 1, Jmeno = "Alena" });
+    }
+}
+
+class Student
+{
+    public int Id { get; set; }
+    public int Rocnik { get; set; }
+    public string Jmeno { get; set; }
+}
