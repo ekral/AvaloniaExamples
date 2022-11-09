2ef9d09b2753fe48c46681e579271097b12788f0
Erik Král <ekral@utb.cz>
2022-11-09 15:12:12 +0100

    upgrade to .NET 7

---
+++ AvaloniaObservableCollection/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaObservableCollection
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
