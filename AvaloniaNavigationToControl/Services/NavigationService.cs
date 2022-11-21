936b4354f2774aec2253b737651c1d25303fae15
Erik Král <ekral@utb.cz>
2022-11-21 15:49:04 +0100

    Rename contentControl

--- AvaloniaNavigationToControl/Services/NavigationService.cs
+++ AvaloniaNavigationToControl/Services/NavigationService.cs
@@ -6,9 +6,9 @@ namespace AvaloniaNavigationToControl.Services
     {
         private readonly ContentControl transitioningContentControl;
 
-        public NavigationService(ContentControl transitioningContentControl)
+        public NavigationService(ContentControl contentControl)
         {
-            this.transitioningContentControl = transitioningContentControl;
+            this.transitioningContentControl = contentControl;
         }
 
         public void Navigate(ContentControl view)
